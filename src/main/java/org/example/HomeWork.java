a087cfdf2573ef1888e5849f92c9af25320d6cbd
Andrew Kuznetsov <56806222+endrewsmith@users.noreply.github.com>
2024-12-10 16:58:57 +0300

    Update HomeWork.java

--- src/main/java/org/example/HomeWork.java
+++ src/main/java/org/example/HomeWork.java
@@ -3,8 +3,15 @@ package org.example;
 
 import lombok.SneakyThrows;
 
+import java.io.BufferedReader;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class HomeWork {
 
@@ -14,11 +21,31 @@ public class HomeWork {
      */
     @SneakyThrows
     public void stepDanceValue(InputStream in, OutputStream out) {
-        out.write('3');
-        out.write('\n');
-        out.flush();
+        // На всякий случай проверим входные параметры
+        if (in == null) {
+            throw new IllegalArgumentException("Входящий поток не может быть null");
+        }
+        if (out == null) {
+            throw new IllegalArgumentException("Выходящий поток не может быть null");
+        }
+        AVLTree tree = new AVLTree();
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+
+        List<Integer> ints = br.lines().
+                flatMap(str -> Arrays.stream(str.split(" ")))
+                .map(Integer::parseInt).collect(Collectors.toList());
+        List<Integer> result = new ArrayList<>(ints.get(2));
+        IntStream.range(1, ints.get(0) + 1).forEach(tree::add);
+        ints.stream().skip(2).forEach(
+                i -> {
+                    tree.change(i);
+                    result.add(tree.uniq());
+                }
+        );
+
+        out.write(result.stream()
+                .map(String::valueOf)
+                .collect(Collectors.joining("\n"))
+                .getBytes());
     }
-
-
-
 }
